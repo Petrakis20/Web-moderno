8fc2dbda9096bc551bed1b52ed8bdfe7892a490d
Your Name <you@example.com>
2022-09-29 21:35:21 -0300

    29/09

---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const modulaA = require('./modulaA');
+const modulaB = require('./modulaB');
+
+console.log(modulaA.ola)
+console.log(modulaA.bemVindo)
+console.log(modulaA.ateLogo)
+console.log(modulaA)
+
+console.log(modulaB.bomDia)
+console.log(modulaB.boaNoite())
+console.log(modulaB)
